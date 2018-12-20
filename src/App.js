5677aee20f75633edbbdfb5a33733f839e1f1e83
Marko <mpecic2010@gmail.com>
2018-12-19 18:10:50 -0700

    started working in toolbar, enabled search through emails for read/ unread

--- src/App.js
+++ src/App.js
@@ -21,54 +21,32 @@ updateKeyInState = (idOfObjectToBeUpdated, theKeyToBeUpdated) => {
 console.log(idOfObjectToBeUpdated, theKeyToBeUpdated);
 
   this.setState(prevState => {
-  return {
-    emails: prevState.emails.reduce((acc, email) => {
-      if (email.id == idOfObjectToBeUpdated) {
-        return [
-          ...acc,
-          {
-            ...itemToBeUpdated,
-            [theKeyToBeUpdated]: !email[theKeyToBeUpdated]
-          }
-        ]
-      }
-      return [...acc, email]
-    }, [])
-  }
-})
+    return {
+      emails: prevState.emails.reduce((acc, email) => {
+        if (email.id == idOfObjectToBeUpdated) {
+          return [
+            ...acc,
+            {
+              ...itemToBeUpdated,
+              [theKeyToBeUpdated]: !email[theKeyToBeUpdated]
+            }
+          ]
+        }
+        return [...acc, email]
+      }, [])
+    }
+  })
 }
-      // const newEmails = []
-      // for (var i = 0; i < prevState.emails.length; i++) {
-      //   const email = prevState.emails[i]
-      //   if (email.id == idOfStarToBeUpdated) {
-      //     newEmails.push({
-      //       ...email,
-      //       starred: !email.starred
-      //     })
-      //   }
-      //   newEmails.push(email)
-      // }
-      // return {
-      //   emails: newEmails
-      // }
-        /* 
-        on each iteration of acc it will check to see if email.id == idOfStarToBeSelected if it is it will return all 
-        prior items in array then update with ...itemToBeUpdated and specificlay update the key starred to value: !value. 
-        i = 0 // []
-        i = 1 // [{ id: 1 }]
-        i = 2 // [{id: 1}, {id: 2}]
-        i = 3 // [{id: 1}, {id: 2}, { id: 3, starred: false }]
-        i = 4 //
-        
-        */
   updateCheckBoxStatus = (checkBoxId)=>{
     console.log(checkBoxId)
   }
   render() {
     return (
       <div className="App">
-        <Toolbar />
-        <MessageList emails={this.state.emails} updateKeyInState={this.updateKeyInState} />
+        <div className="container">
+          <Toolbar emails={this.state.emails}/>
+          <MessageList emails={this.state.emails} updateKeyInState={this.updateKeyInState} />
+        </div>
       </div>
     );
   }
--- src/component/toolbar.js
+++ src/component/toolbar.js
@@ -1,46 +1,56 @@
 import React from 'react'
 
 class Toolbar extends React.Component {
-    render(){
+  // // select all 
+  // selectAll = () => {
+  //   this.props.emails.filter(email => email.checked ==true).length
+  // }
+  render(){
+      console.log(this.props.emails.filter(email => email.checked == true).length)
+      console.log("These are props on toolbar",this.props.emails);
         return(
-        <div className="row toolbar">
+          <div className="row toolbar">
           <div className="col-md-12">
             <p className="pull-right">
-            <span className="badge badge">2</span>
+              <span className="badge badge">{this.props.emails.filter(email => email.read ==true).length}</span>
               unread messages
             </p>
-
-        <a className="btn btn-danger">
-      <i className="fa fa-plus"></i>
-    </a>
-
-    <button className="btn btn-default">
-      <i className="fa fa-minus-square-o"></i>
-    </button>
-
-    <button className="btn btn-default">Mark As Read</button>
-
-    <button className="btn btn-default">Mark As Unread</button>
-
-    <select className="form-control label-select">
-      <option>Apply label</option>
-      <option value="dev">dev</option>
-      <option value="personal">personal</option>
-      <option value="gschool">gschool</option>
-    </select>
-
-    <select className="form-control label-select">
-      <option>Remove label</option>
-      <option value="dev">dev</option>
-      <option value="personal">personal</option>
-      <option value="gschool">gschool</option>
-    </select>
-
-    <button className="btn btn-default">
-      <i className="fa fa-trash-o"></i>
-    </button>
-  </div>
-</div>
+            <a className="btn btn-danger">
+              <i className="fa fa-plus"></i>
+            </a>
+        
+            <button className="btn btn-default">
+              <i className="fa fa-square-o" ></i> Select All Button
+              {/* className={`row message ${this.props.value.read ? "read" :  "unread" `} */}
+            </button>
+        
+            <button className="btn btn-default" disabled="disabled">
+              Mark As Read
+            </button>
+        
+            <button className="btn btn-default" disabled="disabled">
+              Mark As Unread
+            </button>
+        
+            <select className="form-control label-select" disabled="disabled">
+              <option>Apply label</option>
+              <option value="dev">dev</option>
+              <option value="personal">personal</option>
+              <option value="gschool">gschool</option>
+            </select>
+        
+            <select className="form-control label-select" disabled="disabled">
+              <option>Remove label</option>
+              <option value="dev">dev</option>
+              <option value="personal">personal</option>
+              <option value="gschool">gschool</option>
+            </select>
+        
+            <button className="btn btn-default" disabled="disabled">
+              <i className="fa fa-trash-o"></i>
+            </button>
+          </div>
+        </div>
 
             )
     }
